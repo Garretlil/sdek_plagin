68160aac48c4b77a1dd1bfe06169c9e572b38d76
Garretlil <ed763135@gmail.com>
2025-04-02 17:26:02 +0300

    Added clustering for placemarks

--- lib/SdekWindowNotifier.dart
+++ lib/SdekWindowNotifier.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:yandex_maps_mapkit_lite/image.dart' as mapkitImage;
 import 'package:yandex_maps_mapkit_lite/mapkit.dart' as mapkit;
 import 'CdekApi.dart';
 import 'CdekAuth.dart';
+import 'CustomClusterPainter.dart';
 import 'gps_module.dart';
 
 class PointPlaceMark{
@@ -21,19 +21,25 @@ class PointPlaceMark{
   });
 
 }
-
 class SdekWindowNotifier extends ChangeNotifier {
   static const clientId = 'NYnDZhNexvHneGnk29cdGpZuAxwFot6J';
   static const clientSecret = 'RglJK9tAYIUUhP2Dt3NuBChjm7iESwkf';
   late final CdekAuth auth;
   late final CdekApi api;
   List<DeliveryPoint> points = [];
-  List<PointPlaceMark> placemarks=[];
+  List<PointPlaceMark> placemarks = [];
+  mapkit.ClusterizedPlacemarkCollection? clusterizedCollection;
+
   SdekWindowNotifier() {
     auth = CdekAuth(clientId: clientId, clientSecret: clientSecret);
     api = CdekApi(auth);
-    // _loadPoints();
   }
+
+  late final GeoLocator geoLocator;
+  mapkit.MapWindow? mapWindow;
+  double? zoom;
+  mapkit.Point currentPosition = const mapkit.Point(latitude: 59.935493, longitude: 30.327392);
+  final cameraCallback = mapkit.MapCameraCallback(onMoveFinished: (isFinished) {});
 
   Future<void> _loadPoints() async {
     try {
@@ -44,92 +50,42 @@ class SdekWindowNotifier extends ChangeNotifier {
         description: point.name,
         adress: point.address,
       )).toList();
-      //notifyListeners();
     } catch (e) {
       print('Ошибка загрузки пвз: $e');
     }
   }
 
-  late final GeoLocator geoLocator;
-  mapkit.MapWindow? mapWindow;
-  double? zoom;
-  mapkit.Point currentPosition = const mapkit.Point(latitude: 59.935493, longitude: 30.327392);
-  final cameraCallback = mapkit.MapCameraCallback(onMoveFinished: (isFinished) {});
-  // late List<PointPlaceMark> placemarks=[
-  //   PointPlaceMark(latitude: points[0].latitude,
-  //       longitude: points[0].longitude,
-  //       description: points[0].name,
-  //       adress: points[0].address
-  //   )
-  //   PointPlaceMark(
-  //       latitude: 55.76,
-  //       longitude: 37.63,
-  //       description: 'Пункт выдачи СДЭК',
-  //       adress:'Кутузовский проспект д32'),
-  //   PointPlaceMark(
-  //       latitude: 55.66,
-  //       longitude: 37.57,
-  //       description: 'Пункт выдачи СДЭК',
-  //       adress:'Кутузовский проспект д32'),
-  //   PointPlaceMark(
-  //       latitude: 55.56,
-  //       longitude: 37.50,
-  //       description: 'Пункт выдачи СДЭК',
-  //       adress:'Кутузовский проспект д32'),
-  // ];
   Future<void> onMapCreated(mapkit.MapWindow mapWindow) async {
-    _loadPoints();
-    await Future.delayed(Duration(seconds: 5));
+    await _loadPoints();
     this.mapWindow = mapWindow;
-    zoom = mapWindow.map.cameraPosition.zoom+8;
+    zoom = mapWindow.map.cameraPosition.zoom + 8;
 
-    geoLocator = GeoLocator();
-    await geoLocator.setLocation();
+    final clusterIcon = mapkitImage.ImageProvider.fromImageProvider(
+      const AssetImage("assets/sdek_icon.png"),
+    );
 
-    Position? position;
-    try {
-      position = await geoLocator.position;
-    } catch (e) {
-      return;
-    }
-    mapWindow.map.move(mapkit.CameraPosition(
-      mapkit.Point(latitude: position!.latitude, longitude: position.longitude),
-      zoom: zoom!,
-      azimuth: 0.0,
-      tilt: 0.0,
-    ));
-    for (var item in placemarks) {
-      final placeMark = mapWindow.map.mapObjects.addPlacemark()
+    clusterizedCollection = mapWindow.map.mapObjects.addClusterizedPlacemarkCollection(
+      ClusterListenerImpl(),
+    );
+
+    final icon = mapkitImage.ImageProvider.fromImageProvider(
+      const AssetImage("assets/sdek_icon.png"),
+    );
+
+    for (var placemark in placemarks) {
+      clusterizedCollection!.addPlacemark()
         ..geometry = mapkit.Point(
-            latitude: item.latitude,
-            longitude: item.longitude
+          latitude: placemark.latitude,
+          longitude: placemark.longitude,
         )
-        ..setText('${item.description}\n${item.adress}')
-        ..setTextStyle(
-            const mapkit.TextStyle(
-              size: 8.0,
-              color: Colors.black,
-              outlineWidth: 50,
-              outlineColor: Colors.white,
-              placement: mapkit.TextStylePlacement.Right,
-              offset: 2.0,
-              offsetFromIcon: true,
-            )
-        )
-        ..setIcon(mapkitImage.ImageProvider.fromImageProvider(
-            const AssetImage("assets/sdek_icon.png"))
-        )
-        ..setIconStyle(mapkit.IconStyle(scale: 0.5));
+        ..setIcon(icon);
     }
 
-    mapWindow.map.nightModeEnabled = false;
-    print("карта создана: начальный zoom: $zoom");
+    clusterizedCollection!.clusterPlacemarks(clusterRadius: 60.0, minZoom: 5);
 
-    currentPosition = mapWindow.map.cameraPosition.target;
-
+    print("Кластеры добавлены");
     notifyListeners();
   }
-
 
   void setMapWindow(mapkit.MapWindow window) {
     currentPosition = mapWindow!.map.cameraPosition.target;
@@ -159,7 +115,31 @@ class SdekWindowNotifier extends ChangeNotifier {
         tilt: 0.0,
       ),
     );
-    zoom=newZoom;
+    zoom = newZoom;
     notifyListeners();
   }
 }
+
+
+class ClusterListenerImpl implements mapkit.ClusterListener {
+  @override
+  void onClusterAdded(mapkit.Cluster cluster) async {
+    final iconBytes = await ClusterIconPainter(cluster.placemarks.length).getClusterIconBytes();
+    final clusterIcon = mapkitImage.ImageProvider.fromImageProvider(
+      MemoryImage(iconBytes),
+    );
+
+    cluster.appearance.setIcon(clusterIcon);
+  }
+}
+
+
+class ClusterTapListenerImpl implements mapkit.ClusterTapListener {
+  @override
+  bool onClusterTap(mapkit.Cluster cluster) {
+    print("Клик по кластеру с ${cluster.placemarks.length} объектами");
+    return true;
+  }
+}
+
+
